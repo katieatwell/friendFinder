2196dd3876cf86dfeda27656e38f996d9d5e5e35
Katie Atwell <katieatwell@outlook.com>
2017-12-12 02:03:33 +0000

    emptied friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,3 @@
 var friends = [];
 var bodyparser = require("body-parser");
 module.exports = { friends: friends };
-{"name":"Cody","image":"http://i.dailymail.co.uk/i/pix/2015/01/07/2477898D00000578-0-image-a-10_1420651238012.jpg","answer":["","","","","","","","","",""]}{"name":"Katie","image":"http://i.dailymail.co.uk/i/pix/2015/01/07/2477898D00000578-0-image-a-10_1420651238012.jpg","answer":["","","","","","","","","",""]}
