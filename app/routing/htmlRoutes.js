7e013123e95abb5ae4178786f195cfc9ccc39ea4
Katie Atwell <katieatwell@outlook.com>
2017-12-12 02:24:18 +0000

    filepath

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,12 +4,12 @@ var path = require("path");
 module.exports = function(app) {
     //Connect User to HomePage by default
     app.get("/", function(req, res) {
-        res.sendFile(path.join(__dirname + "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
     //Connect User to Survey Page (this will be on click)
     app.get("/friendsurvey", function(req, res) {
-        res.sendFile(path.join(__dirname + "../public/survey.html"));
+        res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
 };
